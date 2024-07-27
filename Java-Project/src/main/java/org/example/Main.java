4f46acef3121950d32a2a932c5fb6b5fbf2b765a
anh0701 <quynhanhphamthi01@gmail.com>
2024-07-27 19:08:27 +0700

    add comment

--- Java-Project/src/main/java/org/example/Main.java
+++ Java-Project/src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 import java.util.Scanner;
 
+/**
+ * using encapsulation (private, public)
+ * using polymorphism (override toString() method)
+ * using abstraction (List<Student> students = new ArrayList<>())
+ */
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
